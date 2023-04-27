7e372e1956809c6155179b2e856db3145950f066
fonsecaaso <mateusfonseca27@hotmail.com>
2023-04-27 15:33:21 -0300

    user balance list endpoint (back)

--- DesafioBack/src/main/java/com/hubla/DesafioBack/controller/UserController.java
+++ DesafioBack/src/main/java/com/hubla/DesafioBack/controller/UserController.java
@@ -1,2 +1,24 @@
-package com.hubla.DesafioBack.controller;public class UserController {
+package com.hubla.DesafioBack.controller;
+
+import com.hubla.DesafioBack.entity.dto.UserDTO;
+import com.hubla.DesafioBack.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping
+    public List<UserDTO> getAll(){
+        return  userService.getAll();
+    }
 }
--- DesafioBack/src/main/java/com/hubla/DesafioBack/service/UserService.java
+++ DesafioBack/src/main/java/com/hubla/DesafioBack/service/UserService.java
@@ -1,2 +1,27 @@
-package com.hubla.DesafioBack.service;public class UserService {
+package com.hubla.DesafioBack.service;
+
+import com.hubla.DesafioBack.entity.User;
+import com.hubla.DesafioBack.entity.dto.UserDTO;
+import com.hubla.DesafioBack.repository.UserRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+    public List<UserDTO> getAll() {
+        List<User> users = userRepository.findAll();
+        List<UserDTO> ans = new ArrayList<>();
+        for(User user:users){
+            UserDTO dto = new UserDTO();
+            BeanUtils.copyProperties(user,dto);
+            ans.add(dto);
+        }
+        return ans;
+    }
 }
